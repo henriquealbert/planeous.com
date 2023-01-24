5aadc8fcc267db9627effcf67fdd3279e98ab857
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-01-24 14:05:37 -0300

    :lipstick: add matine ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,33 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
+
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "../utils/api";
+import { mantineTheme } from "../styles/mantineTheme";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>My SaaS</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={mantineTheme}>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </MantineProvider>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { Button, Flex, Title } from "@mantine/core";
 import { type NextPage } from "next";
+import Link from "next/link";
 
-const Home: NextPage = () => {
-  return <h1>Hello SaaS</h1>;
+const HomePage: NextPage = () => {
+  return (
+    <Flex direction="column" justify="center" align="center" h="100vh">
+      <Title order={1}>Hello SaaS</Title>
+      <div>
+        <Link href="/login">
+          <Button>Go to Login Page</Button>
+        </Link>
+      </div>
+    </Flex>
+  );
 };
 
-export default Home;
+export default HomePage;
