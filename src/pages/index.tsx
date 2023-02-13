a226635e112b872538768e1a3d2eaa73ae4cec12
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-02-13 10:44:17 -0300

    :art: rename home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { type NextPage } from 'next'
 const HomePage: NextPage = () => {
   return (
     <Flex direction="column" justify="center" align="center" h="100vh">
-      <Title order={1}>Welcome to Planeous</Title>
+      <Title order={1}>Planeous.com</Title>
     </Flex>
   )
 }
