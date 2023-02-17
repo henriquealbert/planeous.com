835bafc69bd0e90d050fe6dbddfdf9c0e8104c3f
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-02-17 10:59:02 -0300

    update dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,20 +1,12 @@
-import { Button, Flex, Text, Title } from '@mantine/core'
+import { Flex, Title } from '@mantine/core'
 import { ProtectedRoute } from 'components/PrivateRoute/PrivateRoute'
-import { useAuth } from 'contexts/AuthContext'
 import { type NextPage } from 'next'
-import { signOut } from 'next-auth/react'
 
 const DashboardPage: NextPage = () => {
-  const { sessionData } = useAuth()
-
   return (
     <ProtectedRoute>
-      <Flex direction="column" justify="center" align="center" h="100vh">
+      <Flex direction="column">
         <Title order={1}>This is the dashboard</Title>
-        <Text mb="lg">Hello, {sessionData?.user?.name}</Text>
-        <div>
-          <Button onClick={() => void signOut()}>Sign out</Button>
-        </div>
       </Flex>
     </ProtectedRoute>
   )
