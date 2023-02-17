595a79a4671c49d25d17b3ea07985badc6a40933
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-02-17 10:58:55 -0300

    add header

---
+++ src/components/PrivateRoute/Header/Header.tsx
@@ -0,0 +1,16 @@
+import { Flex, Header as MantineHeader } from '@mantine/core'
+import { MantineLogo } from '@mantine/ds'
+import { AvatarMenu } from './AvatarMenu'
+
+export const Header = () => {
+  return (
+    <MantineHeader height={52} p="xs" display="flex" w="100%">
+      <Flex ml="md">
+        <MantineLogo type="mark" size={30} />
+      </Flex>
+      <Flex justify="flex-end" w="100%" mr="sm">
+        <AvatarMenu />
+      </Flex>
+    </MantineHeader>
+  )
+}
