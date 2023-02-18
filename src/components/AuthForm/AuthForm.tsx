25c1a4feae95832d98423813f3b6e86145b738c9
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-02-18 12:46:30 -0300

    update authform

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,5 +1,5 @@
 import { upperFirst } from '@mantine/hooks'
-import { Anchor, Flex, Paper, Text, Title } from '@mantine/core'
+import { Anchor, Box, Center, Flex, Paper, Text, Title } from '@mantine/core'
 import { GoogleButton } from 'components/SocialButtons/GoogleButton'
 import { signIn } from 'next-auth/react'
 import Link from 'next/link'
@@ -9,36 +9,37 @@ type AuthFormProps = {
 }
 export const AuthForm = ({ type }: AuthFormProps) => {
   return (
-    <>
-      <Title order={1}>This is the {upperFirst(type)} page</Title>
-      <Text>Choose your provider to {upperFirst(type)}:</Text>
+    <Box>
+      <Title align="center" order={1}>
+        {type === 'signup' ? 'Welcome to Planeous!' : 'Welcome again to Planeous!'}
+      </Title>
+      <Text align="center">Choose your provider to {upperFirst(type)}:</Text>
 
-      <Paper radius="md" p="xl" withBorder mt="xl" w={350}>
-        <Flex direction="column" gap={16} mb="md">
-          <GoogleButton
-            size="md"
-            onClick={() => void signIn('google', { callbackUrl: '/dashboard' })}
-          >
-            Connect with Google
-          </GoogleButton>
-        </Flex>
+      <Center>
+        <Paper radius="md" p="xl" withBorder mt="xs" w={350}>
+          <Flex direction="column" gap={16} mb="md">
+            <GoogleButton size="md" onClick={() => void signIn('google', { callbackUrl: '/' })}>
+              Connect with Google
+            </GoogleButton>
+          </Flex>
 
-        {type === 'signup' && (
-          <Text size="xs" w="300px" align="center" mx="auto">
-            By clicking continue, you agree to our Terms of Service and Privacy Policy.
-          </Text>
-        )}
+          {type === 'signup' && (
+            <Text size="xs" w="300px" align="center" mx="auto">
+              By clicking continue, you agree to our Terms of Service and Privacy Policy.
+            </Text>
+          )}
 
-        <Flex justify="center">
-          <Link href={type === 'login' ? '/signup' : '/login'}>
-            <Anchor component="button" type="button" color="dimmed" size="xs">
-              {type === 'signup'
-                ? 'Already have an account? Login'
-                : "Don't have an account? Register"}
-            </Anchor>
-          </Link>
-        </Flex>
-      </Paper>
-    </>
+          <Flex justify="center" mt="xl">
+            <Link href={type === 'login' ? '/signup' : '/login'}>
+              <Anchor component="button" type="button" color="dimmed" size="xs">
+                {type === 'signup'
+                  ? 'Already have an account? Login'
+                  : "Don't have an account? Register"}
+              </Anchor>
+            </Link>
+          </Flex>
+        </Paper>
+      </Center>
+    </Box>
   )
 }
