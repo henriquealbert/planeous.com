61002b0ee359eb26112d19d50acec0dff179c122
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-02-19 18:30:02 -0300

    :lipstick: update contact list page

--- src/pages/app/contacts/index.tsx
+++ src/pages/app/contacts/index.tsx
@@ -1,15 +1,18 @@
 import { Flex, Title } from '@mantine/core'
-import { TableSort } from 'components/contacts/ContactsList'
 import { mockData } from 'components/contacts/mockData'
 import { ProtectedRoute } from 'components/Layouts/PrivateRoute/PrivateRoute'
 import { type NextPage } from 'next'
+import { ContactList } from 'components/contacts/ContactList'
 
 const ContactsPage: NextPage = () => {
   return (
     <ProtectedRoute>
       <Flex direction="column">
-        <Title order={1}>Contacts</Title>
-        <TableSort data={mockData} />
+        <Title order={1} mb="xl">
+          Contacts
+        </Title>
+
+        <ContactList data={mockData} />
       </Flex>
     </ProtectedRoute>
   )
