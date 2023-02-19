93368d7f785e4ddf3119e1b4f1b2e3f5a8633f12
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-02-19 18:30:25 -0300

    :lipstick: update create contact button

--- src/components/contacts/CreateContact.tsx
+++ src/components/contacts/CreateContact.tsx
@@ -1,5 +1,20 @@
 import { Button } from '@mantine/core'
+import { IconPlus } from '@tabler/icons-react'
 
 export const CreateContact = () => {
-  return <Button size="sm">Create Contact</Button>
+  const handleCreateNewContact = () => {
+    prompt('Create new contact modal')
+  }
+  return (
+    <Button
+      leftIcon={<IconPlus size={14} stroke={4} />}
+      size="sm"
+      variant="outline"
+      mb="xl"
+      onClick={handleCreateNewContact}
+      color="gray"
+    >
+      Create contact
+    </Button>
+  )
 }
