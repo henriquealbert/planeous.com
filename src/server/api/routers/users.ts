13af0f07253879b21d461e837d69787fc2a50262
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-02-27 18:04:02 -0300

    :alien: add createMemberAndInvite trpc route

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,3 +1,7 @@
+import type { User } from '@prisma/client'
+import { inviteMembersFormSchema } from 'components/Organization/InviteMembers/validation'
+import { env } from 'env/server.mjs'
+import { sendEmail } from 'server/email/emailClient'
 import { z } from 'zod'
 import { createTRPCRouter, protectedProcedure } from '../trpc'
 
@@ -24,5 +28,40 @@ export const userRouter = createTRPCRouter({
       })
 
       return { user }
+    }),
+  createMemberAndInvite: protectedProcedure
+    .input(inviteMembersFormSchema)
+    .mutation(async ({ input, ctx }) => {
+      const members = await ctx.prisma.$transaction(
+        input.members.map((m) =>
+          ctx.prisma.user.create({
+            data: {
+              email: m.email,
+              organization: {
+                connect: {
+                  id: input.organizationId
+                }
+              }
+            }
+          })
+        )
+      )
+
+      const emailsSent = await sendEmail({
+        to: members.map((m: User) => ({ email: m.email })),
+        subject: 'You have been invited to join Planeous',
+        htmlPart: `
+          <p>Hi there,</p>
+          <p>${
+            ctx.session?.user?.name || 'Someone'
+          } invited you to join his team on Planeous.com</p>
+          <p>Click <a href="${env.APP_HOSTNAME}/signup?from=invite-email">here</a> to join</p>
+          <br />
+          <br />
+          <p>Planeous Team</p>
+        `
+      })
+
+      return { members, emailsSent }
     })
 })
