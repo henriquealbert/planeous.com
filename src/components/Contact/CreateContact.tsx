9416c19211e26c0e887c9b2432c60b0c01271ca7
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-03-02 16:27:51 -0300

    :lipstick: update create contact button

--- src/components/Contact/CreateContact.tsx
+++ src/components/Contact/CreateContact.tsx
@@ -1,22 +1,33 @@
-import { Button } from '@mantine/core'
-import { IconPlus } from '@tabler/icons-react'
 import { useTranslations } from 'next-intl'
+import { Button, Menu } from '@mantine/core'
+import { IconChevronDown, IconPlus, IconUpload } from '@tabler/icons-react'
 
 export const CreateContact = () => {
   const t = useTranslations('Contacts')
 
-  const handleCreateNewContact = () => {
+  const handleCreateSingleContact = () => {
     prompt('Create new contact modal')
   }
 
+  const handleImportCSV = () => {
+    alert('Import CSV')
+  }
+
   return (
-    <Button
-      leftIcon={<IconPlus size={14} stroke={4} />}
-      size="sm"
-      mb="xl"
-      onClick={handleCreateNewContact}
-    >
-      {t('createContact')}
-    </Button>
+    <Menu withinPortal position="bottom-start">
+      <Menu.Target>
+        <Button size="sm" mb="xl" rightIcon={<IconChevronDown size="1rem" stroke={1.5} />}>
+          {t('createContact')}
+        </Button>
+      </Menu.Target>
+      <Menu.Dropdown>
+        <Menu.Item icon={<IconPlus size="1rem" stroke={1.5} />} onClick={handleCreateSingleContact}>
+          Create a single contact
+        </Menu.Item>
+        <Menu.Item icon={<IconUpload size="1rem" stroke={1.5} />} onClick={handleImportCSV}>
+          Import CSV
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
   )
 }
