fe51d52d9fc98b0c2425847c7de28765de0f859e
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-03-03 11:55:11 -0300

    :label: add dropzone types

---
+++ src/types/Dropzone.d.ts
@@ -0,0 +1,16 @@
+export enum ErrorCode {
+  FileInvalidType = 'file-invalid-type',
+  FileTooLarge = 'file-too-large',
+  FileTooSmall = 'file-too-small',
+  TooManyFiles = 'too-many-files'
+}
+
+export interface FileError {
+  message: string
+  code: ErrorCode | string
+}
+
+export interface FileRejection {
+  file: File
+  errors: FileError[]
+}
