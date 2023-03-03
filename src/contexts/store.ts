3603a9b1f5a87765966434e43378b76e4b0222f5
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-03-03 11:56:06 -0300

    :art: add actions to store

--- src/contexts/store.ts
+++ src/contexts/store.ts
@@ -2,11 +2,24 @@ import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { immer } from 'zustand/middleware/immer'
 
+import Router from 'next/router'
+import type { FileWithPath } from '@mantine/dropzone'
+import type { FileRejection } from 'types/Dropzone'
+
 interface Store {
   inviteMembersModal: {
     open: boolean
     openModal: () => void
     closeModal: () => void
+  }
+  createContact: {
+    handleImportCSV: () => void
+    handleCreateSingleContact: () => void
+  }
+  importContact: {
+    isLoading: boolean
+    onDrop: (files: FileWithPath[]) => void
+    onReject: (fileRejections: FileRejection[]) => void
   }
 }
 
@@ -24,6 +37,23 @@ export const useStore = create<Store>()(
           set((state) => {
             state.inviteMembersModal.open = false
           })
+        }
+      },
+      createContact: {
+        handleCreateSingleContact: () => {
+          prompt('Create new contact modal')
+        },
+        handleImportCSV: () => {
+          void Router.push('/app/contacts/import')
+        }
+      },
+      importContact: {
+        isLoading: false,
+        onDrop: (files) => {
+          console.log('drop', files)
+        },
+        onReject: (fileRejections) => {
+          console.log('reject', fileRejections)
         }
       }
     }))
